9af3940868052f02ad2e70d63e53379f74cf660b
mohieey <Mohhiey@gmail.com>
2023-07-07 08:50:55 +0300

    implment simple analyzer with static input

---
+++ main.go
@@ -0,0 +1,29 @@
+package main //all go source files starting with package declaration
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("myapp.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	for {
+		s, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		if strings.Contains(s, "ERROR") {
+			fmt.Println(s)
+		}
+	}
+}
